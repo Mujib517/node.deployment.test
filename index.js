6008019d869623de45cee43dc8b9917922122433
Mujib517 <mujib517@gmail.com>
2017-09-25 19:51:08 +0530

    http server

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+
+
+var http = require("http");
+
+function handelRequest(req, res) {
+    res.write("Up");
+    res.end();
+}
+
+var server = http.createServer(handelRequest);
 
 var port = process.env.PORT | 3000;
 
-app.listen(port, function () {
-    console.log('running');
+server.listen(port, function () {
+    console.log("running...");
 });
 
-app.get('/', function (req, res) {
-    res.send("Up");
-});
+
+// app.listen(port, function () {
+//     console.log('running');
+// });
+
+// app.get('/', function (req, res) {
+//     res.send("Up");
+// });
